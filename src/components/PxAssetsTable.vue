46a30de9542696da7642177f4528dc2f5d18e6b3
Yaakov <dbrionesmtz@yahoo.com>
2021-12-13 11:39:35 -0600

    agregue links

--- src/components/PxAssetsTable.vue
+++ src/components/PxAssetsTable.vue
@@ -29,7 +29,14 @@
         <td>
           <b># {{ a.rank }}</b>
         </td>
-        <td>{{ a.name }}</td>
+        <td>
+          <router-link
+            class="hover:underline text-green-600"
+            :to="{ name: 'coin-detail', params: { id: a.id } }"
+            >{{ a.name }}</router-link
+          >
+          <small class="ml-1 text-gray-500">{{ a.symbol }}</small>
+        </td>
         <td>{{ a.priceUsd | dollar }}</td>
         <td>{{ a.marketCapUsd | dollar }}</td>
         <td
@@ -41,20 +48,33 @@
         >
           {{ a.changePercent24Hr | percent }}
         </td>
-        <td class="hidden sm:block"></td>
+        <td class="hidden sm:block">
+          <px-button @custom-click="goToCoin(a.id)">
+            <span>Detalles</span>
+          </px-button>
+        </td>
       </tr>
     </tbody>
   </table>
 </template>
 
 <script>
+import PxButton from './PxButton.vue'
 export default {
   name: 'PxAssetsTable',
+
+  components: { PxButton },
 
   props: {
     assets: {
       type: Array,
       default: () => [],
+    },
+  },
+
+  methods: {
+    goToCoin(id) {
+      this.$router.push({ name: 'coin-detail', params: { id } })
     },
   },
 }
---
+++ src/components/PxButton.vue
@@ -0,0 +1,19 @@
+<template>
+  <button
+    @click="butonClick"
+    class="bg-transparent hover:bg-green-500 text-green-700 font-semibold hover:text-white py-2 px-2 border border-green-500 hover:border-transparent rounded"
+  >
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'PxButton',
+  methods: {
+    butonClick() {
+      this.$emit('custom-click')
+    },
+  },
+}
+</script>
